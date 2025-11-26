8364435b232b21fc4578f2f24dc1e8f18c68b7b8
ingridmicaa <ingridmicaela03@gmail.com>
2025-11-26 12:25:26 -0500

    soluciòn problema 5

--- EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo05.java
+++ EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo05.java
@@ -7,6 +7,10 @@
  sean ingresados por teclado por el usuario.
  */
 package ejemplos02;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
@@ -14,16 +18,24 @@ package ejemplos02;
 public class Ejemplo05 {
 
     public static void main(String[] args) {
-        // 2. numero, i[0, n]
+         Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        
+       
         int numero;
-        // 3. potencia, i[0, n]
         int potencia;
-        // 6. resultado, i[0, n]
         int resultado;
+        System.out.println("Ingrese un nùmero");
+        // 3. potencia, i[0, n]
+        numero= entrada.nextInt();
+        System.out.println("Ingrese la potencia");
+        potencia= entrada.nextInt();
+        // 6. resultado, i[0, n]
+       
         // numero <-- 4
-        numero = 4;
+       
         // 8. potencia <-- 3
-        potencia = 3;
+        
         // 9. resultado <-- 1
         resultado = 1;
 
@@ -34,6 +46,6 @@ public class Ejemplo05 {
             contador = contador + 1;
         } while (contador <= potencia);
 
-        System.out.printf("%d\n", resultado);
+        System.out.printf("El reultado es: %d\n", resultado);
     }
 }
